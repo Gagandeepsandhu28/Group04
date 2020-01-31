d04f07492c2c5010c1ef22d52695cf76f6be4e9a
Gagan Sandhu <Gsandhu6261@conestogac.on.ca>
2020-01-30 23:25:21 -0500

    AboutUs Page created

---
+++ Week3_FormCreation/Controllers/AboutUsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Week3_FormCreation.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Week3_FormCreation.Controllers
+{
+    public class AboutUsController: Controller
+    {
+
+        public IActionResult Index()
+        {
+            AboutUs aboutUs = new AboutUs
+            {
+                Title = "Make My Food",
+            Body = "Make My Food opened its doors with one goal in mind: Better Pizza." +
+            " We knew that with best ingredients we would create better pizzas. That goal and the promise of " +
+            "best ingredients, better pizza remain true always."
+        };
+          
+
+            return View(aboutUs);
+        }
+    }
+}
--- Week3_FormCreation/Controllers/HomeController.cs
+++ Week3_FormCreation/Controllers/HomeController.cs
@@ -27,7 +27,10 @@ namespace Week3_FormCreation.Controllers
         {
             return View();
         }
-
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
