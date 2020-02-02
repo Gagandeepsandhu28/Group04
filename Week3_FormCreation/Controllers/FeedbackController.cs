e300ceba6db9905c6c1eb65bc203a76f4d7d4761
pramod2310 <pramodjat748@gmail.com>
2020-02-02 13:18:14 -0500

    Feedback form completed by pramod

---
+++ Week3_FormCreation/Controllers/FeedbackController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Week3_FormCreation.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Week3_FormCreation.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(Feedback feedback)
+        {
+            return View(feedback);
+        }
+    }
+}
