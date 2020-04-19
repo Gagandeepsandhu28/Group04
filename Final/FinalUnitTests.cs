d305c29a6cf11fe28e45b8c3c48ef0febae9cab4
pramod2310 <pramodjat748@gmail.com>
2020-04-19 12:53:07 -0400

    12 Unit Test Cases - 1- Store Menu,  7 - Cart Operations - Pramod Jat

--- Final/FinalUnitTests.cs
+++ Final/FinalUnitTests.cs
@@ -118,5 +118,206 @@ namespace Final
             // Assert
             Assert.Equal(expected_result.ToString(), actual_result.ToString());
         }
+                // PRAMOD TEST STARTS HERE
+
+        // VIEW ALL CART ITEMS -> TRUE
+        [Fact]
+        public void ViewCartItems_ViewCartItemsReturnTrue()
+        {
+            //Arrange
+            string expected_result = "True";
+
+            //Act
+            // CREATE CART OBJECT
+            string Cart_session_id = "79163306-fb23-4ed1-8357-723f01a855a3";
+            
+            // CREATE CARTHANDLER OBJECT
+            CartHandler carthandler = new CartHandler();
+            var cartresults = carthandler.ViewCartItems(Cart_session_id);
+
+            bool actual_result = false;
+            if(cartresults.Count()>0)
+            {
+                actual_result = true;
+            }
+
+            // Assert
+            Assert.Equal(expected_result.ToString(), actual_result.ToString());
+        }
+        // VIEW ALL CART ITEMS -> FALSE
+
+        [Fact]
+        public void ViewCartItems_ViewCartItemsReturnFalse()
+        {
+            //Arrange
+            string expected_result = "False";
+
+            //Act
+            // CREATE CART OBJECT
+            string Cart_session_id = "09163306-fb23-4ed1-8357-723f01a855a3";
+
+            // CREATE CARTHANDLER OBJECT
+            CartHandler carthandler = new CartHandler();
+            var cartresults = carthandler.ViewCartItems(Cart_session_id);
+
+            bool actual_result = false;
+            if (cartresults.Count() > 0)
+            {
+                actual_result = true;
+            }
+
+            // Assert
+            Assert.Equal(expected_result.ToString(), actual_result.ToString());
+        }
+
+
+        // VIEW CART SUBTOTAL (ALL ITEMS PRICES EXCEPT BEFORE TAX TOTAL) - TRUE
+        [Fact]
+        public void GetCartSubTotal_GetCartSubTotalDbReturnTrue()
+        {
+            //Arrange
+            string expected_result = "True";
+
+            //Act
+            // CREATE CART OBJECT
+            string Cart_session_id = "79163306-fb23-4ed1-8357-723f01a855a3";
+
+            // CREATE CARTHANDLER OBJECT
+            CartHandler carthandler = new CartHandler();
+            var cartresults = carthandler.GetCartSubTotal(Cart_session_id);
+
+            bool actual_result = false;
+            if (cartresults.Count() > 0)
+            {
+                actual_result = true;
+            }
+
+            // Assert
+            Assert.Equal(expected_result.ToString(), actual_result.ToString());
+        }
+
+        // VIEW CART SUBTOTAL (ALL ITEMS PRICES EXCEPT BEFORE TAX TOTAL) - FALSE
+        [Fact]
+        public void GetCartSubTotal_GetCartSubTotalDbReturnFalse()
+        {
+            //Arrange
+            string expected_result = "True";
+
+            //Act
+            // CREATE CART OBJECT
+            string Cart_session_id = "59163306-fb23-4ed1-8357-723f01a855a3";
+
+            // CREATE CARTHANDLER OBJECT
+            CartHandler carthandler = new CartHandler();
+            var cartresults = carthandler.GetCartSubTotal(Cart_session_id);
+
+            bool actual_result = false;
+            if (cartresults.Count() > 0)
+            {
+                actual_result = true;
+            }
+
+            // Assert
+            Assert.Equal(expected_result.ToString(), actual_result.ToString());
+        }
+
+        // GET STORE ITEM DETAILS  TRUE
+        [Fact]
+        public void GetStoreItemDetails_GetStoreItemDetailsDbReturnTrue()
+        {
+            //Arrange
+            string expected_result = "True";
+
+            //Act
+            // CREATE CART OBJECT
+            int store_id = 0;
+            int store_menu_id = 4;
+
+            StoreMenuHandler storemenu = new StoreMenuHandler();
+            var store_item_details = storemenu.GetStoreItemDetails(store_id, store_menu_id);
+
+            bool actual_result = false;
+            if (store_item_details.Count() > 0)
+            {
+                actual_result = true;
+            }
+
+            // Assert
+            Assert.Equal(expected_result.ToString(), actual_result.ToString());
+        }
+
+        // GET STORE ITEM DETAILS  FALSE
+        [Fact]
+        public void GetStoreItemDetails_GetStoreItemDetailsDbReturnFalse()
+        {
+            //Arrange
+            string expected_result = "False";
+
+            //Act
+            // CREATE CART OBJECT
+            int store_id = 7;
+            int store_menu_id = 4;
+
+            StoreMenuHandler storemenu = new StoreMenuHandler();
+            var store_item_details = storemenu.GetStoreItemDetails(store_id, store_menu_id);
+
+            bool actual_result = false;
+            if (store_item_details.Count() > 0)
+            {
+                actual_result = true;
+            }
+
+            // Assert
+            Assert.Equal(expected_result.ToString(), actual_result.ToString());
+        }
+
+        // GET STORE MENUS - TRUE
+        [Fact]
+        public void GetStoreMenu_GetStoreMenuDbReturnTrue()
+        {
+            //Arrange
+            string expected_result = "True";
+
+            //Act
+            // CREATE CART OBJECT
+            int store_id = 0;
+      
+
+            StoreMenuHandler storemenu = new StoreMenuHandler();
+            var store_menu = storemenu.GetStoreMenu(store_id);
+
+            bool actual_result = false;
+            if (store_menu.Count() > 0)
+            {
+                actual_result = true;
+            }
+
+            // Assert
+            Assert.Equal(expected_result.ToString(), actual_result.ToString());
+        }
+        // GET STORE MENUS - FALSE
+        [Fact]
+        public void GetStoreMenu_GetStoreMenuDbReturnFalse()
+        {
+            //Arrange
+            string expected_result = "False";
+
+            //Act
+            // CREATE CART OBJECT
+            int store_id = 7;
+
+
+            StoreMenuHandler storemenu = new StoreMenuHandler();
+            var store_menu = storemenu.GetStoreMenu(store_id);
+
+            bool actual_result = false;
+            if (store_menu.Count() > 0)
+            {
+                actual_result = true;
+            }
+
+            // Assert
+            Assert.Equal(expected_result.ToString(), actual_result.ToString());
+        }
     }
 }
